937839995fe7ec41e5676a575d096b0827b13c29
qiang.wang01 <qiang.wang01@jiduauto.com>
2024-09-18 19:32:06 +0800

    update

---
+++ hello/go.mod
@@ -0,0 +1,9 @@
+module github.com/Oliverwqcwrw/aolifu-go/hello
+
+go 1.23.0
+
+require (
+	github.com/Oliverwqcwrw/aolifu-go/greetings v0.0.0
+)
+
+require golang.org/x/example/hello v0.0.0-20240916213319-f3623ff43ac2 // indirect
--- hello/hello.go
+++ hello/hello.go
@@ -3,19 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/Oliverwqcwrw/aolifu-go/greetings"
-	"github.com/google/go-cmp/cmp"
-	"golang.org/x/example/stringutil"
+	"golang.org/x/example/hello/reverse"
 	"log"
 	"os"
 )
 
 func init() {
-	var (
-		home   = os.Getenv("HOME")
-		user   = os.Getenv("USER")
-		gopath = os.Getenv("GOPATH")
-	)
-	fmt.Println(home, user, gopath)
+	home := os.Getenv("HOME")
+	user := os.Getenv("USER")
+	gopath := os.Getenv("GOPATH")
 
 	if user == "" {
 		log.Fatal("$USER not set")
@@ -30,41 +26,11 @@ func init() {
 }
 
 func main() {
-	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
-	fmt.Println(stringutil.Reverse("Hello"))
-
-	log.SetPrefix("greetings: ")
-	log.SetFlags(0)
-
-	// message, err := greetings.Hello("Oliver")
-	// A slice of names.
+	fmt.Println(reverse.String("Hello"))
 	names := []string{"Gladys", "Samantha", "Darrin"}
-
-	// Request greeting messages for the names.
 	messages, err := greetings.Hellos(names)
-
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Get a greeting message and print it.
-	//message := greetings.Hello("Gladys")
 	fmt.Println(messages)
-
-	x := []int{1, 2, 3}
-	x = append(x, 4, 5, 6)
-	fmt.Println(x)
-
-	x = []int{1, 2, 3}
-	y := []int{4, 5, 6}
-	x = append(x, y...)
-	fmt.Println(x)
-	log.Println("Stats In One Minute.", map[string]interface{}{
-		"statsName": "statsName",
-		"statsKey":  "statsKey",
-		"SUM":       "SUM",
-		"TPS":       fmt.Sprintf("%.2f", 2.3),
-		"AVGPT":     23,
-	})
-
 }
